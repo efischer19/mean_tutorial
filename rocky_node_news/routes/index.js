b52e8522b56c92364e66e1154cfcd48db5ce94c9
efischer0119 <efischer0119@gmail.com>
2015-04-28 18:50:47 -0400

    finishing up the backend, making routes and such

--- rocky_node_news/routes/index.js
+++ rocky_node_news/routes/index.js
@@ -30,4 +30,74 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
+/*post parameter*/
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+/*GET a specific post*/
+router.get('/posts/:post', function(req, res, next) {
+	req.post.populate('comments', function(err, post) {
+		if (err) { return next(err); }
+
+		res.json(post);
+	}); 
+});
+
+/*PUT an upvote on a pos*/
+router.put('/posts/:post/upvote', function(req, res, next) {
+	req.post.upvote(function(err, post){
+		if(err) { return next(err); }
+	
+		res.json(post);
+	});
+});
+
+/*POST comments*/
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+/*comment parameter*/
+router.param('comment', function(req, res, next, id) {
+	var query = Comment.findById(id);
+
+	query.exec(function (err, comment){
+		if (err) { return next(err); }
+		if (!post) { return next(new Error('can\'t find comment')); }
+
+		req.comment = comment;
+		return next;
+	});
+});
+
+/*PUT an upvote on a comment*/
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+	req.comment.upvote(function(err, coment){
+		if(err) {return next(err); }
+
+		res.json(post);
+	});
+});
+
 module.exports = router;
