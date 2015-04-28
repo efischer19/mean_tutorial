1a3dc0bb6b956ca26335f2a0f7680616057a00ba
efischer0119 <efischer0119@gmail.com>
2015-04-28 18:19:16 -0400

    Opening REST routes on the backend, site is functional again

--- rocky_node_news/app.js
+++ rocky_node_news/app.js
@@ -5,15 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 var mongoose = require('mongoose');
 require('./models/Posts');
 require('./models/Comments');
-mongoose.conect('mongodb://localhost/news');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+mongoose.connect('mongodb://localhost/news');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
